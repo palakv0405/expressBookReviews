c9903a09d4c0919a79c84139acbbedd306714f46
palakv0405 <palak.vispute231@vit.edu>
2026-04-21 16:51:42 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,79 +1,28 @@
-const express = require('express');
-const public_users = express.Router();
+const axios = require("axios");
 
-let books = require("./booksdb.js");
-
-// user DB
-let users = [];
-
-const isValid = (username) => username.length > 0;
-
-const doesExist = (username) => {
-    return users.some(user => user.username === username);
+const getBooks = async () => {
+  const res = await axios.get("http://localhost:5000/");
+  return res.data;
 };
 
-// Task 1: Get all books
-public_users.get('/', (req, res) => {
-    return res.send(JSON.stringify(books, null, 2));
-});
+const getBookByISBN = async (isbn) => {
+  const res = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+  return res.data;
+};
 
-// Task 2: Get by ISBN
-public_users.get('/isbn/:isbn', (req, res) => {
-    const isbn = req.params.isbn;
-    return res.send(books[isbn]);
-});
+const getBooksByAuthor = async (author) => {
+  const res = await axios.get(`http://localhost:5000/author/${author}`);
+  return res.data;
+};
 
-// Task 3: Get by author
-public_users.get('/author/:author', (req, res) => {
-    const author = req.params.author;
-    let result = {};
+const getBooksByTitle = async (title) => {
+  const res = await axios.get(`http://localhost:5000/title/${title}`);
+  return res.data;
+};
 
-    for (let key in books) {
-        if (books[key].author === author) {
-            result[key] = books[key];
-        }
-    }
-
-    return res.send(result);
-});
-
-// Task 4: Get by title
-public_users.get('/title/:title', (req, res) => {
-    const title = req.params.title;
-    let result = {};
-
-    for (let key in books) {
-        if (books[key].title === title) {
-            result[key] = books[key];
-        }
-    }
-
-    return res.send(result);
-});
-
-// Task 5: Get reviews
-public_users.get('/review/:isbn', (req, res) => {
-    const isbn = req.params.isbn;
-    return res.send(books[isbn].reviews);
-});
-
-// Task 6: Register
-public_users.post('/register', (req, res) => {
-    const { username, password } = req.body;
-
-    if (!username || !password) {
-        return res.status(400).json({ message: "Missing fields" });
-    }
-
-    if (doesExist(username)) {
-        return res.status(409).json({ message: "User exists" });
-    }
-
-    if (isValid(username)) {
-        users.push({ username, password });
-        return res.json({ message: "Registered successfully" });
-    }
-});
-
-module.exports.general = public_users;
-module.exports.users = users;
+module.exports = {
+  getBooks,
+  getBookByISBN,
+  getBooksByAuthor,
+  getBooksByTitle
+};
